6574787a9e1c87fcc4f63a478acb657a37e02336
abiodun emma <abiodunemma769@gmail.com>
2025-01-20 16:51:55 +0100

    sign_up_page_completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home(){
   return (
- <div className="h-screen bg-cover sm:bg-no-repeat md:bg-fixed bg-center "
+ <div className="min-h-screen overflow-aut bg-cover sm:bg-no-repeat md:bg-fixed bg-center "
  style={{ 
   backgroundImage: "url('/img.png')",
   }}>
@@ -11,42 +11,72 @@ export default function Home(){
     <div className="justify-center
     bg-black h-full bg-opacity-50 p-6 rounded-lg shadow-sm focus:ring-blue-500 focus:border=blue-500
     ">
-      <div className="bg-white  rounded-md p-6">
-      <p className="mb-2 ">Let get you started</p>
-      <h1 className=" bold-18 "><strong>Create any account</strong></h1>
+      <div className="bg-white  rounded-md p-6  pb-80 ">
+      <p className="mb-2 text-sm ">Let get you started</p>
+      <h1 className=" bold-8 mb-6 "><strong>Create any account</strong></h1>
       <form className="space-y-4 ">
       
-  <label className="block  text-sm font-medium text-black-700" htmlFor= "name"> Your Name</label>
+  <label className="block  text-sm font-medium  mb-[-10px] text-black-700"  htmlFor= "name"> Your Name</label>
      <input 
      id="your name"
      type="text"
      placeholder="Enter your name"
-     className="mt-1 block w-full px-4 py-2 border-gray rounded-lg  "
+     className="mt-1 block w-full px-4 py-2 border-gray rounded-lg  border-2 border-gray-300 "
      />
 
-<label className="block  text-sm font-medium text-black-700" htmlFor= "Email"> Email</label>
+<label className="block  text-sm font-medium  text-black-700" htmlFor= "Email"> Email</label>
      <input 
      id="Email"
      type="email"
      placeholder="Enter your Email"
-     className="mt-1 block w-full px-4 py-2 border-gray-300 rounded-lg  "
+     className="mt-1 block w-full px-4 py-2  rounded-lg border-2 border-gray-300 mb-[-10px]  "
      />
 
-<label className="block  text-sm font-medium text-black-700" htmlFor= "Passowrd"> Password</label>
+<label className="block  text-sm font-medium  text-black-700" htmlFor= "Passowrd"> Password</label>
      <input 
      id="Password"
      type="password"
      placeholder="Enter your password"
-     className="mt-1 block w-full px-4 py-2 border-gray-300 rounded-lg  "
+     className="mt-1 block w-full px-4 py-2 rounded-lg  border-2 border-gray-300"
      />
          <Button
          type="Button"
-         title="Sign up"
+         title="Get started"
          icon="/user.svg"
-         variant= "btn_dark_green"
+         variant= "btn_black"
+   
          />
            </form>
+           <div className="p-4">
+            <hr className="border-b-1 border-black w-full">
+            </hr>
+            </div>
+           <Button
+             type="button"
+             title="Sign up with Google"
+             icon="/gogle.png"
+             variant= "btn_white"
+           />
+
+<Button
+             type="button"
+             title="Sign up with Google"
+             icon="/facebook.png"
+             variant= "btn_white"
+           />
+
+<Button
+             type="button"
+             title="Sign up with Google"
+             icon="/apple.png"
+             variant= "btn_white"
+           />
+<div className="mt-12  text-center ">
+  <p className="text-sm"> Already have an account? <strong>LOGIN HERE</strong> </p>
+
+</div>
     </div>
+
 
  </div>
 </div>
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import Image from 'next/image'
 
-const Button = () => {
-  return (
-    <div>Button</div>
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: string;
+}
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+    const variantClasses = {
+        btn_black: 'bg-green-90 text-white border-black-500 py-4 hover:bg-black',
+        btn_white: 'bg-white-90 text-black border-black-500 py-4 hover:bg-gray',
+        btn_red: 'bg-red-500 text-white border-red-500 hover:bg-red-600',
+        btn_blue: 'bg-blue-500 text-white border-blue-500 hover:bg-blue-600',
+        // Default fallback class
+        default: 'bg-gray-300 text-gray-800 border-gray-300 hover:bg-gray-400',
+      };
+
+
+  // Get the classes based on the variant, fallback to 'default' if not found
+  const buttonClasses = variantClasses[variant] || variantClasses.default;
+return (
+<button className={`flexCenter  gap-3 w-full rounded-lg border mt-4 ${buttonClasses}`}
+type={type}
+>
+{icon && <Image src={icon} alt={title} width={24} height={24} />}
+<label className='bold-16 whitespace-nowrap'>{title}</label>
+</button>
   )
 }
 
