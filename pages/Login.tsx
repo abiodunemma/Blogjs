9f5b5a3c5080509045edf9eafcc7b7f679064794
abiodun emma <abiodunemma769@gmail.com>
2025-01-21 12:39:39 +0100

    Login_page

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,16 +1,16 @@
 import Button from "@/components/Button";
 import Image from "next/image";
+import '../styles/login.css';
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div
-      className="min-h-screen bg-cover bg-center md:bg-fixed"
+    <div className="min-h-screen bg-cover bg-no-repeat bg-fixed sm:bg-auto md:bg-cover lg:bg-cover sm:bg-top md:bg-center"
       style={{
-        backgroundImage: "url('/img.png')",
-      }}
-    >
-      <div className="min-h-screen flex flex-col md:flex-row items-center bg-black bg-opacity-50">
+        backgroundImage: "url('/bg-img.png')" }}>
+     
+     
+     <div className="min-h-screen flex flex-col md:flex-row items-center bg-black bg-opacity-60">
         {/* Left Section */}
         <div className="hidden md:flex flex-1 justify-center items-center">
           <div className="text-white text-center px-6">
@@ -24,24 +24,12 @@ export default function Home() {
         {/* Right Section */}
         <div className="flex-1 bg-white rounded-lg p-6 md:max-w-md w-4/5 h-auto mx-auto">
 
-          <p className="mb-2 text-sm">Let’s get you started</p>
-          <h1 className="text-lg font-bold mb-6">Create an Account</h1>
+          <p className="mb-2 text-sm text-gray-400">WELCOME BACK</p>
+          <h1 className="text-sm font-bold mb-6">Login in to your Account</h1>
           <form className="space-y-4">
+           
             <label
-              className="block text-sm font-medium text-black"
-              htmlFor="name"
-            >
-              Your Name
-            </label>
-            <input
-              id="your name"
-              type="text"
-              placeholder="Enter your name"
-              className="mt-1 block w-full px-4 py-2 border-gray-300 rounded-lg border-2"
-            />
-
-            <label
-              className="block text-sm font-medium text-black"
+              className="block text-sm font-medium text-gray-500"
               htmlFor="Email"
             >
               Email
@@ -54,7 +42,7 @@ export default function Home() {
             />
 
             <label
-              className="block text-sm font-medium text-black"
+              className="block text-sm font-medium text-gray-500"
               htmlFor="Password"
             >
               Password
@@ -65,10 +53,24 @@ export default function Home() {
               placeholder="Enter your password"
               className="mt-1 block w-full px-4 py-2 rounded-lg border-2 border-gray-300"
             />
+           
+           <div className="py-4 flex justify-between items-center w-full">
+  <div className="flex items-center space-x-2">
+    <input 
+      type="checkbox" 
+      id="rememberMe" 
+      className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+    />
+    <label htmlFor="rememberMe" className="text-sm text-gray-700">Remember me</label>
+  </div>
+  
+  <span className="text-sm text-blue-500 cursor-pointer">Forgot password?</span>
+</div>
+
 
             <Button
               type="Button"
-              title="Get started"
+              title="CONTINUE"
               icon="/user.svg"
               variant="btn_black"
             />
@@ -103,12 +105,12 @@ export default function Home() {
             <p className="text-sm">
 
               Already have an account?
-              <Link href="/Login"><strong>LOGIN HERE</strong>
-              </Link> 
+              <strong><Link href="page">Signup
+              </Link></strong>
             </p>
           </div>
         </div>
       </div>
-    </div>
+</div>
   );
 }
